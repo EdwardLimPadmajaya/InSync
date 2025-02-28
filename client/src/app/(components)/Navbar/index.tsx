3bd7add1f9a92d7f65dcc50a85a069dc74d35d35
Edward3957503 <edwardpadmajaya@gmail.com>
2025-02-28 21:37:44 +1100

    Added Sidebar component

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { Search, Settings } from 'lucide-react'
-import Link from 'next/link'
+import React from 'react';
+import { Search, Settings } from 'lucide-react';
+import Link from 'next/link';
 const Navbar = () => {
     return (
         <div className='flex items-center justify-between w-full px-4 py-4 bg-white dark:bg-dark-bg'>
